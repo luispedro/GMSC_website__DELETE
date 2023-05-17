133391c572c38cd392fa1408fbf5453483072ea0
cocodyq <1057696203@qq.com>
2023-05-17 15:57:36 +0800

    Update help page

--- src/Help.elm
+++ src/Help.elm
@@ -21,7 +21,7 @@ main =
     , update = \_ _ -> ((), Cmd.none)
     , subscriptions = \_ -> Sub.none
     , view = \_ ->
-        { title = "GMSC:Home"
+        { title = "GMSC:Help"
         , body =
             [ CDN.stylesheet
             , CDN.fontAwesome
@@ -49,60 +49,79 @@ content =
     span [id "help"]
         [Markdown.toHtml [] """
 ## Overview
-Description
+The global microbial smORFs catalogue (GMSC) is an integrated, consistently-processed, smORFs catalogue of the microbial world, combining 63,410 publicly available metagenomes and 87,920 high-quality isolated microbial genomes from the [ProGenomes database](https://progenomes.embl.de/).
+
+A total of 4.5 billion smORFs were used to build the catalog. After 100%-identity redundancy removal, we obtained a 100AA non-redundant catalog with 965 million sequences. Further, the smORFs were clustered at 90% amino acid identity resulting in 288 million 90AA smORFs catalog.
+
 ## Benefits and Features
-Description
+**Integration:**
+
+- GMSC is available as a web resource that displays each smORFs for browsing their integrated annotations:
+  - clusters
+  - taxonomy classification
+  - habitat assignment
+  - quality assessment
+  - conserved domain annotation
+  - cellular localization prediction
+
+**Main purpose of GMSC**:
+- Expand smORFs sets of global microbiomes with comprehensive annotation
+- Analyse of ecological distribution patterns across taxonomy and global habitats
+- Annotate smORFs of microbial genomes or genes with the resource
+
 ## Searching
-Description
+GMSC-mapper is provided as a search tool to query sequences. Users can provide a genome or gene sequence and it will return a set of smORFs with complete annotations that match the 90AA smORFs in GMSC.
+
+GMSC-mapper can also be downloaded and run locally, please see details in [Github page](https://github.com/BigDataBiology/GMSC-mapper)
+
+figure
+
 ## Identifiers
 smORFs in the catalog are identified with the scheme `GMSC10.100AA.XXX_XXX_XXX` or `GMSC10.90AA.XXX_XXX_XXX`. The initial `GMSC10` indicates the version of the catalog (Global Microbial smORFs Catalog 1.0). The `100AA` or `90AA` indicates the amino acid identity of the catalog. The `XXX_XXX_XXX` is a unique numerical identifier (starting at zero). Numbers were assigned in order of increasing number of copies. So that the greater the number, the greater number of copies of that peptide were present in the raw data. 
 ## Data acquistion
-Description
+63,410 metagenomes published before January 1, 2020 were downloaded from the European Nucleotide Archive ([ENA](https://www.ebi.ac.uk/ena/browser/)). The criteria for selecting metagenomes are: 
+- the taxonomic ID of the sample is 408169, that is, the taxonomic ID for metagenome, or the taxonomic ID is the descendant of 408169 in the taxonomic tree
+- the library source field of experiments is "METAGENOMIC". Samples contain at least 2 million reads produced by Illumina instrument.
+
+87,920 high-quality microbial genomes were downloaded from the [ProGenomes v2 database](https://progenomes.embl.de/).
+
 ## Method
 A figure overview
-#### Read processing and assembly
-Description
-#### Identification of smORFs
-Description
-#### Protein family generation
-Description
+#### Assembly of contigs and prediction of smORFs
+The reads &gt; 60 bps were processed after trimming positions with quality &lt; 25 using [NGLess](https://github.com/ngless-toolkit/ngless). Filtered reads were assembled into contigs using [Megahit](https://github.com/voutcn/megahit).
+
+We used a modified version of [Prodigal](https://github.com/hyattpd/Prodigal) to predict ORFs &gt;= 15 bps.  The ORFs that &lt;= 300 bps were considered smORFs.
+
+#### Cluster generation
+All predicted smORFs were removed redundancy with 100% amino acid sequence identity. Then they were clustered with 90% amino acid identity and 90% coverage using [Linclust](https://github.com/soedinglab/MMseqs2).
+
 #### Taxonomy & Habitat annotation
-Description
+** Taxonomy annotation:**
+
+The taxonomy of assembled contigs encoding the small proteins was annotated using [MMSeqs2](https://github.com/soedinglab/MMseqs2) against the [GTDB database](https://gtdb.ecogenomic.org/). 
+
+we recorded the taxonomy of smORFs based on the taxonomy of the contigs of metagenomes or genomes of [Progenome2 database](https://progenomes.embl.de/) from which the smORFs were predicted. Subsequently, we assign taxonomy for 100AA and 90AA smORFs using the lowest common ancestor (LCA), ignoring the un-assigned rank to make it more specific.
+
+**Habitat annotation:**
+
+The metadata of samples was manually curated starting from annotations in the [NCBI Biosample database](https://www.ncbi.nlm.nih.gov/biosample/?term=) and corresponding manuscripts. Based on the environment ontology of [EMBL-EBI Ontology Lookup Service](https://www.ebi.ac.uk/ols4) and host identifier in [NCBI taxonomy database](https://www.ncbi.nlm.nih.gov/taxonomy/?term=), we defined 157 specific habitats classification in total and divided them into 8 major categories: mammal gut, anthropogenic, other-human, other-animal, aquatic, human gut, soil/plant, and other. 
+
+We recorded the habitats of smORFs according to the samples they originated from.
+
 #### Conserved domain annotation
-Description
+The representative sequences of 90AA smORFs clusters were searched against [NCBI CDD database](https://www.ncbi.nlm.nih.gov/cdd/) by RPS-blast. Hits with an e-value maximum of 0.01 and at least 80% of coverage of PSSM's length were retained and considered significant.
+
 #### Cellular localization prediction
-Description
+[TMHMM2](https://services.healthtech.dtu.dk/services/TMHMM-2.0/) was run on the representative sequences of 90AA smORFs clusters. 
+
+[SignalP-4.1](https://services.healthtech.dtu.dk/services/SignalP-4.1/) was run on the representative sequences of 90AA smORFs clusters both with 'gram+' and 'gram-' modes.
+
 #### Quality assessment
 Description
 ## Refrence
 Description
 """ ]
-{-
-content: Html msg
-content = 
-    div (Center.styles "800px")
-        [ h1 [] [ text "Overview" ]
-        , a [style "margin-top" "1em"] [ text "description" ]
-        , h1 [style "margin-top" "1em"] [ text "Benefits and Features" ]     
-        , a [style "margin-top" "1em"] [ text "description" ]
-        , h1 [style "margin-top" "1em"] [ text "Searching" ]
-        , h2 [style "margin-top" "1em"] [ text "GMSC-mapper" ]
-        , h1 [style "margin-top" "1em"] [ text "Identifiers" ]
-        , h1 [style "margin-top" "1em"] [ text "Data acquistion" ]   
-        , a [style "margin-top" "1em"] [ text "description" ]
-        , h1 [style "margin-top" "1em"] [ text "Method" ]
-        , a [style "margin-top" "1em"] [ text "A figure overview" ] 
-        , h2 [style "margin-top" "1em"] [ text "Read processing and assembly" ]
-        , h2 [style "margin-top" "1em"] [ text "Identification of smORFs" ]
-        , h2 [style "margin-top" "1em"] [ text "Protein family generation" ]
-        , h2 [style "margin-top" "1em"] [ text "Taxonomy & Habitat annotation" ]
-        , h2 [style "margin-top" "1em"] [ text "Conserved domain annotation" ]
-        , h2 [style "margin-top" "1em"] [ text "Cellular localization prediction" ]
-        , h2 [style "margin-top" "1em"] [ text "Quality estimation" ]
-        , h1 [style "margin-top" "1em"] [ text "c" ]
-        , h1 [style "margin-top" "1em"] [ text "API" ]
-        ]
--}
+
 -- header
 
 
