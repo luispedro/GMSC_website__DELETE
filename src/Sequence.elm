1bd1b2860740a0398c78a9c6456d7df9ada4a81a
cocodyq <1057696203@qq.com>
2023-05-19 17:22:18 +0800

    Add sequence page

---
+++ src/Sequence.elm
@@ -0,0 +1,81 @@
+module Sequence exposing (..)
+import Html exposing (..)
+import Html.Attributes as HtmlAttr
+import Html.Attributes exposing (..)
+import Browser
+import Dict
+import Markdown
+import View exposing (View)
+
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+
+import Shared exposing (..)
+
+
+main: Program () () Never
+main =
+    Browser.document
+    { init = \_ -> ((), Cmd.none)
+    , update = \_ _ -> ((), Cmd.none)
+    , subscriptions = \_ -> Sub.none
+    , view = \_ ->
+        { title = "GMSC:smORF"
+        , body =
+            [ CDN.stylesheet
+            , CDN.fontAwesome
+            , Html.node "link"
+                [ HtmlAttr.rel "stylesheet"
+                , HtmlAttr.href "style.css"
+                ]
+                []
+            , Grid.containerFluid []
+                [ Grid.simpleRow
+                    [ Grid.col []
+                        [ Shared.header
+                        , identifier
+                        , content
+                        , Html.hr [] []
+                        , Shared.footer
+                        ]
+                    ]
+                ]
+            ]
+        }
+    }
+
+-- main text
+
+
+identifier : Html msg
+identifier =
+    span [id "sequence"]
+        [Markdown.toHtml [] """
+# GMSC10.100AA.000_000_001
+""" ]
+
+content : Html msg
+content = table [] [ tr [] [ td [id "info"] [text "protein sequence"]
+                           , td [] [text "MAAAAAAAAAADAGDEDAAFTVDSAEGHELAWHAVQELEYLSD"]
+                           ]
+                    , tr [id "alt"] [ td [id "info"] [text "Length"]
+                            , td [] [text "43"]
+                            ]
+                    , tr [] [ td [id "info"] [text "Taxonomic assignment"]
+                            , td [] [text "f__Streptosporangiaceae"]
+                            ]
+                    , tr [id "alt"] [ td [id "info"] [text "Habitat"]
+                            , td [] [text "soil"]
+                            ]
+                    , tr [] [ td [id "info"] [text "Protein cluster"]
+                            , td [] [a [ href "https://guide.elm-lang.org" ] [ text "GMSC10.90AA.000_000_083" ]]
+                            ]
+                    , tr [id "alt"] [ td [id "info"] [text "Conserved domain"]
+                            , td [] [text "-"]
+                            ]
+                    , tr [] [ td [id "info"] [text "Cellular localization"]
+                            , td [] [text "-"]
+                            ]
+                    ]
