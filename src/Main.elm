53ecfbffecc757cd94bde0f0986c399709e0fbf7
cocodyq <1057696203@qq.com>
2023-06-14 22:13:24 +0800

    ENH add url for topbar

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,9 @@ import Html.Attributes exposing(..)
 import Html.Attributes as HtmlAttr
 import Html.Events exposing (..)
 import Browser
+import Browser.Navigation as Nav
+import Url exposing (Url)
+import Url.Parser as Parser exposing (Parser, (</>), custom, fragment, map, oneOf, top)
 import Dict
 import Markdown
 import View exposing (View)
@@ -34,17 +37,20 @@ import Download
 import Help
 import About
 
+type alias Model =
+  { key : Nav.Key
+  , page : Page
+  }
 
-
-type Model =
-    HomeModel Home.Model
-    | SequenceModel Sequence.Model
-    | ClusterModel Cluster.Model
-    | MapperModel Mapper.Model
-    | BrowseModel Browse.Model
-    | DownloadModel 
-    | HelpModel
-    | AboutModel
+type Page =
+    Home Home.Model
+    | Sequence Sequence.Model
+    | Cluster Cluster.Model
+    | Mapper Mapper.Model
+    | Browse Browse.Model
+    | Download 
+    | Help
+    | About
 
 type Msg
     = HomeMsg Home.Msg
@@ -57,105 +63,125 @@ type Msg
     | GoToDownload
     | GoToHelp
     | GoToAbout
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model = 
+    Sub.none
 
 -- INIT
-init : flags -> ( Model, Cmd Msg )
-init _ =
-    ( HomeModel Home.initialModel
-    , Cmd.none
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { key = key
+      , page = Home Home.initialModel
+      }
+      ,Cmd.none
     )
 
 -- UPDATE
-
-update : Msg -> Model -> ( Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = case msg of
-    HomeMsg Home.SubmitIdentifier -> case model of
-        HomeModel (Home.IdentifierQuery hm) ->
+    HomeMsg Home.SubmitIdentifier -> case model.page of
+        Home (Home.IdentifierQuery hm) ->
             if String.startsWith "GMSC10.100AA" hm.idcontent
               then
                 let
                   (sm, cmd) = Sequence.initialState hm.idcontent
-                in ( SequenceModel sm, Cmd.map SequenceMsg cmd )
+                in ( { model | page = Sequence sm } , Cmd.map SequenceMsg cmd )
             else  
                 let
                   (sm, cmd) = Cluster.initialState hm.idcontent
-                in ( ClusterModel sm, Cmd.map ClusterMsg cmd )
+                in ( { model | page = Cluster sm } , Cmd.map ClusterMsg cmd )
         _ -> ( model, Cmd.none )
 
-    HomeMsg Home.SubmitSequence -> case model of
-        HomeModel (Home.IdentifierQuery hm) ->
+    HomeMsg Home.SubmitSequence -> case model.page of
+        Home (Home.IdentifierQuery hm) ->
             let
                 (mm, cmd) = Mapper.initialState hm.seqcontent
-            in ( MapperModel mm, Cmd.map MapperMsg cmd )
+            in ( { model | page = Mapper mm } , Cmd.map MapperMsg cmd )
         _ -> ( model, Cmd.none )
 
     GoToHome ->
-        ( HomeModel Home.initialModel, Cmd.none )
+        ({ model | page = Home Home.initialModel },Cmd.none)
 
     GoToBrowse ->
-        ( BrowseModel Browse.initialModel, Cmd.none )
+        ({ model | page = Browse Browse.initialModel },Cmd.none)
     
     GoToDownload ->
-        ( DownloadModel, Cmd.none )
+        ({ model | page = Download },Cmd.none)
 
     GoToHelp ->
-        ( HelpModel, Cmd.none )
+        ({ model | page = Help },Cmd.none)
 
     GoToAbout ->
-        ( AboutModel, Cmd.none )   
+        ({ model | page = About },Cmd.none)
 
-    HomeMsg m -> case model of
-        HomeModel hm ->
+    HomeMsg m -> case model.page of
+        Home hm ->
             let
                 (nhm, cmd) = Home.update m hm
             in
-                ( HomeModel nhm, Cmd.map HomeMsg cmd )
+                ( { model | page = Home nhm }, Cmd.map HomeMsg cmd )
         _ -> ( model, Cmd.none )
 
-    SequenceMsg m -> case model of
-        SequenceModel sm ->
+    SequenceMsg m -> case model.page of
+        Sequence sm ->
             let
                 (nqm, cmd) = Sequence.update m sm
             in
-                ( SequenceModel nqm, Cmd.map SequenceMsg cmd )
+                ( { model | page = Sequence nqm }, Cmd.map SequenceMsg cmd )
         _ -> ( model, Cmd.none )
 
-    ClusterMsg m -> case model of
-        ClusterModel sm ->
+    ClusterMsg m -> case model.page of
+        Cluster sm ->
             let
                 (nqm, cmd) = Cluster.update m sm
             in
-                ( ClusterModel nqm, Cmd.map ClusterMsg cmd )
+                ( { model | page = Cluster nqm } , Cmd.map ClusterMsg cmd )
         _ -> ( model, Cmd.none )
 
-    MapperMsg m -> case model of
-        MapperModel sm ->
+    MapperMsg m -> case model.page of
+        Mapper sm ->
             let
                 (nqm, cmd) = Mapper.update m sm
             in
-                ( MapperModel nqm, Cmd.map MapperMsg cmd )
+                ( { model | page = Mapper nqm } , Cmd.map MapperMsg cmd )
         _ -> ( model, Cmd.none )
 
-    BrowseMsg m -> case model of
-        BrowseModel bm ->
+    BrowseMsg m -> case model.page of
+        Browse bm ->
             let
                 (nbm, cmd) = Browse.update m bm
             in
-                ( BrowseModel nbm, Cmd.map BrowseMsg cmd )
-        _ -> ( model, Cmd.none )    
+                ( { model | page = Browse nbm } , Cmd.map BrowseMsg cmd )
+        _ -> ( model, Cmd.none )  
 
-main: Program () Model Msg
+    LinkClicked urlRequest ->
+      case urlRequest of
+        Browser.Internal url ->
+          ( model
+          , Nav.pushUrl model.key (Url.toString url)
+          )
+        Browser.External href ->
+          ( model
+          , Nav.load href
+          )
+
+    UrlChanged url ->
+      changeRouteTo (fromUrl url) model
+
 main =
-    Browser.document
+  Browser.application
     { init = init
+    , view = view
     , update = update
     , subscriptions = subscriptions
-    , view = view
+    , onUrlRequest = LinkClicked
+    , onUrlChange = UrlChanged
     }
 
 view : Model -> Browser.Document Msg
@@ -182,28 +208,75 @@ view model = { title = "GMSC:Home"
         }
 
 viewModel : Model -> Html Msg
-viewModel model = case model of
-    HomeModel m ->
+viewModel model = case model.page of
+    Home m ->
         Home.viewModel m
             |> Html.map HomeMsg
-    SequenceModel m ->
+    Sequence m ->
         Sequence.viewModel m
             |> Html.map SequenceMsg
-    ClusterModel m ->
+    Cluster m ->
         Cluster.viewModel m
             |> Html.map ClusterMsg
-    MapperModel m ->
+    Mapper m ->
         Mapper.viewModel m
             |> Html.map MapperMsg        
-    BrowseModel m ->
+    Browse m ->
         Browse.viewModel m
             |> Html.map BrowseMsg     
-    DownloadModel ->
+    Download ->
         Download.viewModel 
-    HelpModel ->
+    Help ->
         Help.viewModel 
-    AboutModel ->
+    About ->
         About.viewModel
+
+parser : Parser (Page -> a) a
+parser =
+    oneOf
+        [ Parser.map (Home Home.initialModel) Parser.top
+        , Parser.map (Browse Browse.initialModel) (Parser.s "browse")
+        , Parser.map Download (Parser.s "downloads")
+        , Parser.map Help (Parser.s "help")
+        , Parser.map About (Parser.s "about")
+        ]
+
+fromUrl : Url -> Maybe Page
+fromUrl url =
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+        |> Parser.parse parser
+
+changeRouteTo : Maybe Page -> Model -> ( Model, Cmd Msg )
+changeRouteTo page model =
+  case page of
+    Just (Home home) ->
+         ({ model | page = Home Home.initialModel },Cmd.none)
+            
+    Just (Browse browse) ->
+        ({ model | page = Browse Browse.initialModel },Cmd.none)
+
+    Just Download ->
+        ({ model | page = Download },Cmd.none)
+        
+    Just Help ->
+        ({ model | page = Help },Cmd.none)
+
+    Just About ->
+        ({ model | page = About },Cmd.none)
+
+    Just (Sequence sequence) ->
+        {-({ model | page = Sequence (Sequence.initialState a) },Cmd.none) -} 
+        (model,Cmd.none)
+    
+    Just (Cluster cluster) ->
+        {-({ model | page = Cluster (Cluster.initialState a) },Cmd.none)-}
+        (model,Cmd.none)
+
+    Just (Mapper mapper) ->
+        {-({ model | page = Mapper (Mapper.initialState a) },Cmd.none)-} 
+        (model,Cmd.none) 
+    Nothing ->
+        (model,Cmd.none)
 
 -- header
 
@@ -212,11 +285,11 @@ header : Html Msg
 header =
     div [id "topbar"]
         [Grid.simpleRow
-            [ Grid.col [] [ Html.a [href "#", onClick GoToHome] [Html.text "Home"]] 
-            , Grid.col [] [ Html.a [href "#", onClick GoToBrowse] [Html.text "Browse"]] 
-            , Grid.col [] [ Html.a [href "#", onClick GoToDownload] [Html.text "Downloads"]]
-            , Grid.col [] [ Html.a [href "#", onClick GoToHelp] [Html.text "Help"]]
-            , Grid.col [] [ Html.a [href "#", onClick GoToAbout] [Html.text "About&Contact"]]
+            [ Grid.col [] [ Html.a [href "/home", onClick GoToHome] [Html.text "Home"]] 
+            , Grid.col [] [ Html.a [href "/browse", onClick GoToBrowse] [Html.text "Browse"]] 
+            , Grid.col [] [ Html.a [href "/downloads", onClick GoToDownload] [Html.text "Downloads"]]
+            , Grid.col [] [ Html.a [href "/help", onClick GoToHelp] [Html.text "Help"]]
+            , Grid.col [] [ Html.a [href "/about", onClick GoToAbout] [Html.text "About&Contact"]]
             ]
         ]
 
